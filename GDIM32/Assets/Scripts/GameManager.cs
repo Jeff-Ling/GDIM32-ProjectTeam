236cafd029c385fc8147d9df8b98da93f13f89dd
Jiefu Ling <jeffling0603@gmail.com>
2022-01-31 02:51:32 -0800

    Update
    GameManager is available right now.
    Still need sprite for bullet

---
+++ GDIM32/Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject player1;
+    public GameObject player2;
+
+    public GameObject[] enemys;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // No Player Alive
+        if (checkPlayerAvailability())
+        {
+            // Game Lose
+        }
+
+        // No Enemy Alive
+        if (checkEnemyAvailability())
+        {
+            // Game Win
+        }
+    }
+
+    private bool checkPlayerAvailability()
+    {
+        return (player1 == null && player2 == null);
+    }
+
+    private bool checkEnemyAvailability()
+    {
+        bool NoManAlive = true;
+
+        for (int i = 0; i < enemys.Length; i++)
+        {
+
+            //If someone one is alive
+            if (enemys[i] == null)
+            {
+                NoManAlive = false;
+            }
+        }
+
+        return NoManAlive;
+    }
+}
