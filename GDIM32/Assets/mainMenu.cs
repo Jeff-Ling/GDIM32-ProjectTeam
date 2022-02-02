f8ef3da26fe6ecebbfc4f765bf82ebab865a761a
Kastorp-S <ieoliasong2013@outlook.com>
2022-02-02 07:38:31 -0800

    update
    Menu completed;

---
+++ GDIM32/Assets/mainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    public void playButton ()
+    {
+        // SceneManager.LoadScene("MAP");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void quitButton()
+    {
+        Debug.Log("Quit Success.");
+        Application.Quit();
+    }
+}
+
